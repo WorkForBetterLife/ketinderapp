f1d9965322a7035781d2b0e944acf40a5de36695
Work For better Life <workfbetterlife@gmail.com>
2022-09-04 13:13:03 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+        this is the first commit, please stay tuned for more
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
